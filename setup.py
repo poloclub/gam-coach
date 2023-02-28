1d2f67331835aeaab7b29b19223a100e97b52aa6
Jay Wang <jay@zijie.wang>
2023-02-28 17:48:31 -0500

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     name="gamcoach",
     packages=find_packages(include=["gamcoach", "gamcoach.*"]),
     url="https://github.com/xiaohk/gam-coach",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
